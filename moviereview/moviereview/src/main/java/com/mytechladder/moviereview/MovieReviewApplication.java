c74394046437505878cae431e80706bed8c31466
Reshika Samala <samala.reshika@gmail.com>
2019-12-11 18:26:03 -0800

    Initial Commit

---
+++ moviereview/moviereview/src/main/java/com/mytechladder/moviereview/MovieReviewApplication.java
@@ -0,0 +1,13 @@
+package com.mytechladder.moviereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewApplication.class, args);
+	}
+
+}
